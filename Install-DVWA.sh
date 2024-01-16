ded387378ca8068d704fd0e978328d289542f9f2
IamCarron <47905563+IamCarron@users.noreply.github.com>
2024-01-16 18:54:53 +0100

    Update Install-DVWA.sh

--- Install-DVWA.sh
+++ Install-DVWA.sh
@@ -160,5 +160,5 @@ echo -e "Username: \033[93madmin\033[0m"
 echo -e "Password: \033[93mpassword\033[0m"
 # End of the installer
 echo
-final_message=$(get_language_message "\033[91mWith ♡ by Iamcarron" "\033[91mCon ♡ by Iamcarron")
+final_message=$(get_language_message "\033[91mWith ♡ by IamCarron" "\033[91mCon ♡ by IamCarron")
 echo -e "$final_message"
